02eb777b502ee0fc2994a392b24405e474b0fcaf
GreenCurve <64778398+GreenCurve@users.noreply.github.com>
2024-11-11 21:21:04 +0100

    added image collection for pdf

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pptx import Presentation
-import pdfplumber
+import pymupdf
 
 def pptx_converter(presentation):
 
@@ -29,31 +29,54 @@ def pptx_converter(presentation):
 
 
 
-def pdf_converter(pdf):
+def pdf_converter(pdf_file):
 
-    page_text = []
-    for page in pdf.pages:
-        text = page.extract_text()
-        page_text.append(text)
-        # Process images and non-text elements, as needed
+    pages_text = []
+    pages_images = []
+
+    for page_num in range(len(pdf_file)):
+        page = pdf_file[page_num]
+
+        text = page.get_text("text")
+        pages_text.append(text)
+
+        image_list = page.get_images(full=True)
+
+
+
+        for img_index, img in enumerate(image_list):
+            xref = img[0]
+            base_image = pdf_file.extract_image(xref)
+            image_bytes = base_image["image"]
+
+            # Save the image to a file
+            image_filename = f"page_{page_num + 1}_image_{img_index + 1}.png"
+            with open("Logs/" + image_filename, "wb") as img_file:
+                img_file.write(image_bytes)
+
+            pages_images.append(image_filename)
+
+
+
+
 
     pages_content = []
-    for page_num, page_content in enumerate(pdf.pages):
+    for page_num in range(len(pdf_file)):
         pages_content.append({
             "slide_number": page_num + 1,
-            "text": page_text[page_num],
-            # "images": images_list
+            "text": pages_text[page_num],
+             "images": pages_images[page_num]
         })
 
     return pages_content
 
 
 presentation = Presentation("N_28_Explitsitnaya_pamyat.pptx")
-pdf = pdfplumber.open("Class06-GradientDescent-New.pdf")
+pdf = pymupdf.open("Class06-GradientDescent-New.pdf")
 
 # a = pptx_converter(presentation)
 a = pdf_converter(pdf)
 
-with open("report.txt", "w",encoding="utf-8") as my_file:
+with open("Logs/report.txt", "w",encoding="utf-8") as my_file:
     for each in a:
         my_file.write(str(each) + '\n')
